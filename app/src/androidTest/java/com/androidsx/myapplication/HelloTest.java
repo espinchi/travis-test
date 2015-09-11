86df09aed4a28d1538d6f197b4a3ae1e8e2c8a79
Pablo Pera <pablo.sx@gmail.com>
2015-09-11 17:27:10 -0400

    First tests. One failing

---
+++ app/src/androidTest/java/com/androidsx/myapplication/HelloTest.java
@@ -0,0 +1,18 @@
+package com.androidsx.myapplication;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class HelloTest extends ActivityInstrumentationTestCase2<GooglePlayServicesActivity> {
+
+    public HelloTest() {
+        super(GooglePlayServicesActivity.class);
+    }
+
+    public void testFoo() {
+        assertEquals("ola ke ase", "ola ke ase");
+    }
+
+    public void testBar() {
+        assertEquals("ola ke ase", "hola que haces");
+    }
+}
