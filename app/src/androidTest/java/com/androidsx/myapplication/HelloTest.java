032f1aa1ce7d8a391655a48e6b2e19d7d55e59cb
Pablo Pera <pablo.sx@gmail.com>
2015-09-12 09:41:15 -0400

    Added missing annotation

--- app/src/androidTest/java/com/androidsx/myapplication/HelloTest.java
+++ app/src/androidTest/java/com/androidsx/myapplication/HelloTest.java
@@ -25,6 +25,7 @@ public class HelloTest extends ActivityInstrumentationTestCase2<GooglePlayServic
         assertEquals("ola ke ase", "ola ke ase");
     }
 
+    @Test
     public void testBar() {
         assertEquals("ola ke ase", "hola que haces");
     }
