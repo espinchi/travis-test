407989e8b96c73b459ed82cf585aae52df20d039
Pablo Pera <pablo.sx@gmail.com>
2015-09-14 18:43:28 -0400

    Integrated Fabric

--- app/src/main/java/com/androidsx/myapplication/GooglePlayServicesActivity.java
+++ app/src/main/java/com/androidsx/myapplication/GooglePlayServicesActivity.java
@@ -1,7 +1,19 @@
 package com.androidsx.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 
 public class GooglePlayServicesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fabric.with(this, new Crashlytics());
+    }
 }
