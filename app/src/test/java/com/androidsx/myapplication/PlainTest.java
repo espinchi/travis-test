38157ca1bd23598be78a417249d12a1af20cb73c
Pablo Pera <pablo.sx@gmail.com>
2015-09-14 18:13:47 -0400

    Added plain junit (passing) test

---
+++ app/src/test/java/com/androidsx/myapplication/PlainTest.java
@@ -0,0 +1,14 @@
+package com.androidsx.myapplication;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class PlainTest {
+
+    @Test
+    public void passingTest() {
+        assertEquals("hola", "hola");
+    }
+}
