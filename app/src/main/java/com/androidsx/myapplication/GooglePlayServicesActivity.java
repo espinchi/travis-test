401d1aa9ec1b0ce15003acbdca8d13a9b13c5305
Pablo Pera <pablo.sx@gmail.com>
2015-09-14 21:54:28 -0400

    Retrolambda and jdk8. Version 1.3.1

--- app/src/main/java/com/androidsx/myapplication/GooglePlayServicesActivity.java
+++ app/src/main/java/com/androidsx/myapplication/GooglePlayServicesActivity.java
@@ -2,6 +2,7 @@ package com.androidsx.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -15,5 +16,11 @@ public class GooglePlayServicesActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         Fabric.with(this, new Crashlytics());
+        lambdaFun();
+    }
+
+    private void lambdaFun() {
+        Runnable r = () -> Log.i("GooglePlayServicesActivity", "hey lambda");
+        r.run();
     }
 }
